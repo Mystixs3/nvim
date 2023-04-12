058f0c790bd84612a1702bf5fe89181e61423b2d
Mystixs3 <67374773+Mystixs3@users.noreply.github.com>
2023-04-13 00:21:49 +0100

    Switch to using tabs for indentation

--- lua/mystixs3/core/options.lua
+++ lua/mystixs3/core/options.lua
@@ -5,9 +5,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs and indendentation
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
 opt.autoindent = true
 
 -- line wrapping

1ba9002b70d0801acc34ca1e7a672ea2a4141326
Mystixs3 <67374773+Mystixs3@users.noreply.github.com>
2023-04-13 00:22:17 +0100

    Fix indentation to match new settings

--- lua/mystixs3/plugins/init.lua
+++ lua/mystixs3/plugins/init.lua
@@ -1,13 +1,19 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-    config = function() require("mason").setup() end,
-  },
-  { "rose-pine/neovim", name = "rose-pine" },
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-  { "tpope/vim-fugitive" },
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+		config = function() require("mason").setup() end,
+	},
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{ "tpope/vim-fugitive" },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("indent_blankline").setup {}
+		end,
+	},
 }
--- lua/mystixs3/plugins/telescope.lua
+++ lua/mystixs3/plugins/telescope.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.1",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function() 
-      require('telescope').load_extension('fzf')
-    end,
-  },
-  {
-    "nvim-telescope/telescope-fzf-native.nvim", -- Faster fuzzy finding using a native fuzzy finder
-    build = "make"
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function() 
+			require('telescope').load_extension('fzf')
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim", -- Faster fuzzy finding using a native fuzzy finder
+		build = "make"
+	}
 }
